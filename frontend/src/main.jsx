8886f4d40c5c337dc7661236e1e8b0717559a7e0
sharmaanshul007 <sasansanshul@gmail.com>
2024-11-09 05:03:58 +0530

    added the backend api for getTopStreak and getTopScore

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { store , persistor} from './redux/Store.jsx'
 import App from './App.jsx'
-
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <PersistGate persistor={persistor}>
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>
+  </PersistGate>
 )
