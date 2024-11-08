78e920eb4ae7d3913ede057a191c69d9a0b18729
pecee06 <priyanshu.choudhury2406@outlook.com>
2024-11-09 01:27:02 +0530

    add user model & user controller

--- backend/src/utils/ApiResponse.js
+++ backend/src/utils/ApiResponse.js
@@ -1,13 +1,10 @@
-class ApiResponse{
-    constructor({
-        statusCode,
-        message,
-        data={}
-    }){
+class ApiResponse {
+    constructor({ statusCode, message = "", data = {} }) {
         this.statusCode = statusCode
         this.message = message
         this.data = data
+        this.success = statusCode < 400
     }
 }
 
-module.exports = ApiResponse
+export default ApiResponse
