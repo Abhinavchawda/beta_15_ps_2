6a4b6ff8486b993eedf45b99ed9e5362dc538b56
pecee06 <priyanshu.choudhury2406@outlook.com>
2024-11-09 08:29:33 +0530

    redux-persist module reinstall

--- frontend/src/redux/Store.js
+++ frontend/src/redux/Store.js
@@ -2,7 +2,7 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import {persistReducer} from 'redux-persist';
 import persistStore from 'redux-persist/es/persistStore';
 import storage from 'redux-persist/lib/storage';
-import userReducer from './user';
+import userReducer from './user.js';
 const persistConfig = {
     key:'root',
     storage,
