b3d8955242ad1cd9c1faf9b31a5d815ff509889d
Aryan Bargal <aryan.gorde4@gmail.com>
2024-11-10 06:24:24 +0530

    pull

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,24 @@
 import Meditation from "./components/Meditation"
 import MeditationDetail from "./components/MeditationDetail"
 import Games from "./pages/Games"
-
 import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Chat from "./pages/chat"
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/chat" element={<Chat />} />
-        <div>
-          <Meditation />
-          <MeditationDetail />
-          <Games></Games>
-        </div>
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </Router>
+      <div>
+        <Meditation />
+        <MeditationDetail />
+        <Games></Games>
+      </div>
+    </>
   )
 }
 
